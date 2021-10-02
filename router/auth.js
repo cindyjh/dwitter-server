4bf1644aea20f5bc5d454d9d7e1f9de20e3772c1
joohee <xorjsqnfl@gmail.com>
2021-10-03 00:06:41 +0900

    Refactor: 로그인 API 리팩토링
    - 사용자 정보 validation 보강 및 중복 제거

--- router/auth.js
+++ router/auth.js
@@ -7,23 +7,27 @@ import { validate } from '../middleware/validator.js'
 
 const router = express.Router()
 
-const validateUser = [
+const validateCredential = [
     // 5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.
-    body('username').trim().notEmpty().withMessage('username required')
-        .isLength({min: 5, max: 20}).matches(/^[a-z0-9_-]+$/).withMessage('5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.'),
+    body('username').trim().isLength({min: 5, max: 20}).matches(/^[a-z0-9_-]+$/).withMessage('5~20자의 영문 소문자, 숫자와 특수기호(_),(-)만 사용 가능합니다.'),
     // 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.
-    body('password').trim().notEmpty().withMessage('password required')
-        .isLength({min: 8, max: 16}).withMessage('8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.'),
-    body('name').trim().optional(),
-    body('email').trim().optional().isEmail().withMessage('이메일 형식에 맞지 않습니다.'),
-    body('url').trim().optional().isURL().withMessage('올바른 url 형식을 입력해주세요.')
+    body('password').trim().isLength({min: 8, max: 16}).withMessage('8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.'),
+    validate,
+]
+
+const validateSignup = [
+    ...validateCredential,
+    body('name').trim().notEmpty().withMessage('이름이 필요합니다.'),
+    body('email').trim().isEmail().normalizeEmail().withMessage('이메일 형식에 맞지 않습니다.'),
+    body('url').trim().isURL().withMessage('url 형식이 아닙니다.').optional({nullable: true, checkFalsy: true}),
+    validate,
 ]
 
 // POST /auth/signup
-router.post('/signup', validateUser, validate, authController.signup)
+router.post('/signup', validateSignup, authController.signup)
 
 // POST /auth/login
-router.post('/login', validateUser, validate, authController.login)
+router.post('/login', validateCredential, authController.login)
 
 // GET /auth/me
 router.get('/me', validate, authController.me)
