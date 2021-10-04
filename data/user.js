5743056f0cff182f05c01fa8c8f3e2a7bf37b9c6
joohee <xorjsqnfl@gmail.com>
2021-10-04 23:29:44 +0900

    Feat: User Model 만들어 연결하기

--- data/user.js
+++ data/user.js
@@ -1,26 +1,52 @@
-import { db } from "../db/database.js"
+import SQ from 'sequelize'
+import { db, sequelize } from "../db/database.js"
+
+const DataTypes = SQ.DataTypes
+const User = sequelize.define('user', {
+    id: {
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        unique: true,
+        autoIncrement: true,
+        allowNull: false,
+    },
+    username: {
+        type: DataTypes.STRING(45),
+        unique: true,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+    },
+    name: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+    },
+    url: DataTypes.TEXT,
+}, {
+    // don't forget to enable timestamps!
+    timestamps: true,
+})
 
 export async function findByUsername(username) {
-    return db.execute('SELECT * from users WHERE username=?', [ username ])
-        .then((result) => {
-            return result[0][0]
-        })
+    return User.findOne({ where: {
+        username
+    }})
 }
 
 export async function findById(id) {
-    return db.execute('SELECT * from users WHERE id=?', [ id ])
-        .then((result) => {
-            return result[0][0]
-        })
+    return User.findByPk(id)
 }
 
-export async function create(userInfo) {
-    const { username, password, name, email, url } = userInfo
-    return db.execute(
-        'INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)', 
-        [ username, password, name, email, url ]
-    ).then(result => {
-        return result[0].insertId
+export async function create(user) {
+    return User.create(user).then(data => {
+        console.log(data)
+        return data.dataValues.id
     })
 }
 
--- db/database.js
+++ db/database.js
@@ -6,7 +6,7 @@ export const sequelize = new SQ.Sequelize(database, user, password, {
     host,
     dialect,
     port,
-    logging: (...msg) => console.log(msg), // Displays all log function call parameters
+    // logging: (...msg) => console.log(msg), // Displays all log function call parameters
 })
 
 
